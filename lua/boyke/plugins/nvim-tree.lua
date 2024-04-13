9dff915029568d904e95d8d38d3e68731bc3e0c0
Boyke Newalsing <boyke2448@users.noreply.github.com>
2024-04-13 21:41:19 +0200

    Add focus on nvim tree short cut

--- lua/boyke/plugins/nvim-tree.lua
+++ lua/boyke/plugins/nvim-tree.lua
@@ -50,6 +50,7 @@ return {
 
     keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    keymap.set("n", "<C-f>", "<cmd>NvimTreeFocus<CR>", { desc = "Focus on Nvim Tree" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end
